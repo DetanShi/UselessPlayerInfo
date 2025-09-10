a9192f450c07c983da7c7fa2dad699d493d85277
Detan Shi <detan.shi@gmail.com>
2025-09-09 19:26:13 -0500

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,128 @@
+﻿using Dalamud.Bindings.ImGui;
+using Dalamud.Game.Command;
+using Dalamud.Game.Gui;
+using Dalamud.Game.Text;
+using Dalamud.Interface.ImGuiNotification;
+using Dalamud.Interface.Windowing;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+using Lumina.Excel.Sheets;
+using SamplePlugin.Windows;
+
+
+namespace SamplePlugin;
+
+public sealed class Plugin : IDalamudPlugin
+{
+    [PluginService] internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
+    [PluginService] internal static ITextureProvider TextureProvider { get; private set; } = null!;
+    [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
+    [PluginService] internal static IClientState ClientState { get; private set; } = null!;
+    [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
+    [PluginService] internal static IPluginLog Log { get; private set; } = null!;
+    [PluginService] internal static IChatGui ChatGui { get; private set; } = null!;
+    [PluginService] internal static IToastGui ToastGui { get; private set; } = null!;
+
+    private const string CommandName = "/openmenu";
+    private const string CommandName2 = "/helloworld";
+
+
+    public Configuration Configuration { get; init; }
+
+    public readonly WindowSystem WindowSystem = new("SamplePlugin");
+    private ConfigWindow ConfigWindow { get; init; }
+    private MainWindow MainWindow { get; init; }
+
+    public Plugin()
+    {
+        Configuration = PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
+
+        // You might normally want to embed resources and load them from the manifest stream
+        var goatImagePath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "goat.png");
+
+        ConfigWindow = new ConfigWindow(this);
+        MainWindow = new MainWindow(this, goatImagePath);
+
+        WindowSystem.AddWindow(ConfigWindow);
+        WindowSystem.AddWindow(MainWindow);
+
+        CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
+        {
+            HelpMessage = "A useful message to display in /xlhelp"
+        });
+
+        CommandManager.AddHandler(CommandName2, new CommandInfo(OnCommand)
+        {
+            HelpMessage = "Say Hello World and show current zone"
+        });
+
+        // Tell the UI system that we want our windows to be drawn throught he window system
+        PluginInterface.UiBuilder.Draw += WindowSystem.Draw;
+
+        // This adds a button to the plugin installer entry of this plugin which allows
+        // toggling the display status of the configuration ui
+        PluginInterface.UiBuilder.OpenConfigUi += ToggleConfigUi;
+
+        // Adds another button doing the same but for the main ui of the plugin
+        PluginInterface.UiBuilder.OpenMainUi += ToggleMainUi;
+
+        // Add a simple message to the log with level set to information
+        // Use /xllog to open the log window in-game
+        // Example Output: 00:57:54.959 | INF | [SamplePlugin] ===A cool log message from Sample Plugin===
+        Log.Information($"===A cool log message from {PluginInterface.Manifest.Name}===");
+    }
+
+    public void Dispose()
+    {
+        // Unregister all actions to not leak anythign during disposal of plugin
+        PluginInterface.UiBuilder.Draw -= WindowSystem.Draw;
+        PluginInterface.UiBuilder.OpenConfigUi -= ToggleConfigUi;
+        PluginInterface.UiBuilder.OpenMainUi -= ToggleMainUi;
+
+        WindowSystem.RemoveAllWindows();
+
+        ConfigWindow.Dispose();
+        MainWindow.Dispose();
+
+        CommandManager.RemoveHandler(CommandName);
+    }
+
+    private void OnCommand(string command, string args)
+    {
+
+        switch (command)
+        {
+            case CommandName:
+                // In response to the slash command, toggle the display status of our main ui
+                MainWindow.Toggle();
+                break;
+            case CommandName2:
+                var territoryId = Plugin.ClientState.TerritoryType;
+                if (Plugin.DataManager.GetExcelSheet<TerritoryType>().TryGetRow(territoryId, out var territoryRow))
+                {
+                    var zoneName = territoryRow.PlaceName.Value.Name.ToString();
+
+                    // Print to chat
+                    ChatGui.Print(new XivChatEntry
+                    {
+                        Message = $"Hello World! You are in: {zoneName}",
+                        Type = XivChatType.SystemMessage
+                    });
+                    // Toast notification
+                    ToastGui.ShowQuest($"Hello World! You are in: {zoneName}");
+                }
+                else
+                {
+                    ToastGui.ShowQuest($"Hello World! You are not in a valid zone.");
+                }
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    public void ToggleConfigUi() => ConfigWindow.Toggle();
+    public void ToggleMainUi() => MainWindow.Toggle();
+}
