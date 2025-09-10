2afe5feec5aaa5f9f9803c00260d6f2797f65cea
Detan Shi <detan.shi@gmail.com>
2025-09-09 20:43:44 -0500

    Update MainWindow.cs

--- Windows/MainWindow.cs
+++ Windows/MainWindow.cs
@@ -15,7 +15,7 @@ public class MainWindow : Window
     // The user will see "My Amazing Window" as window title,
     // but for ImGui the ID is "My Amazing Window##With a hidden ID"
     public MainWindow(Plugin plugin)
-        : base("My Amazing Window##With a hidden ID", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
+        : base("Useless Info", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
     {
         SizeConstraints = new WindowSizeConstraints
         {
@@ -42,7 +42,7 @@ public class MainWindow : Window
         // Normally a BeginChild() would have to be followed by an unconditional EndChild(),
         // ImRaii takes care of this after the scope ends.
         // This works for all ImGui functions that require specific handling, examples are BeginTable() or Indent().
-        using (var child = ImRaii.Child("SomeChildWithAScrollbar", Vector2.Zero, true))
+        using (var child = ImRaii.Child("InfoWindow", Vector2.Zero, true))
         {
             // Check if this child is drawing
             if (child.Success)
